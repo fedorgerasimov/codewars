c343421b21edeeb8310efbafe0ac23d5116b495e
fedorgerasimov <1031577@mai.ru>
2022-01-25 11:41:04 +0300

    add description

--- Sum of positive.js
+++ Sum of positive.js
@@ -6,9 +6,9 @@ Note: if there is nothing to sum, the sum is default to 0
 
 function positiveSum(arr) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= 0) {
-            sum += arr [i];
+    for (let i = 0; i < arr.length; i++) { // setup loop to go through array of given length
+        if (arr[i] >= 0) {                 // if arr[i] is greater than zero   
+            sum += arr [i];                // // add arr[i] to total
         }
     }
     return sum;
