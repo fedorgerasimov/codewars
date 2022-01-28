028101faea3ae769b29f8e5d40c0dc472384d6d5
fedorgerasimov <1031577@mail.ru>
2022-01-28 14:44:03 +0300

    renamed

---
+++ String_repeat.js
@@ -0,0 +1,7 @@
+// Write a function called repeatStr which repeats 
+//the given string string exactly n times.
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+console.log(repeatStr(6, "I")); // "IIIIII"
+console.log(repeatStr(5, "Hello")); // "HelloHelloHelloHelloHello"
