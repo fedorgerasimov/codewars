ce9a52e70d697c41d2eeeb346570a083946fff89
fedorgerasimov <1031577@mail.ru>
2022-01-28 15:04:24 +0300

    8kyu

---
+++ Convert_Number_to_String.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a number into a string.
+//123 --> "123"
+function numberToString(num) {
+   return String(num);
+ }
+ let string = numberToString(6);
+ console.log(typeof(string));
+
+
