3c88ee3ba1024bf6be6e617f4564319d5e0e9d05
fedorgerasimov <1031577@mail.ru>
2022-01-31 20:28:16 +0300

    8kyu

--- Needle_in_the_Haystack.js
+++ Needle_in_the_Haystack.js
@@ -12,3 +12,13 @@ function findNeedle(haystack) {
     return 'found the needle at position ' + haystack.indexOf('needle');
   }
 console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']));
+
+//another solution
+function findNeedle(haystack) {
+    for (let i = 0; i <= haystack.length; i++) {
+        if (haystack[i] === 'needle') {
+            return 'found the needle at position ' + [i]; // return `found the needle at position ${i}`;
+        }
+    }
+} 
+console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']));  
