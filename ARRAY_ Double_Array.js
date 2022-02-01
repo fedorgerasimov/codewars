b55c3414da43f32606b371f9df3cea584f538053
fedorgerasimov <1031577@mail.ru>
2022-02-01 19:45:35 +0300

    8kyu

---
+++ ARRAY_ Double_Array.js
@@ -0,0 +1,23 @@
+/*Given an array of integers, return a new array with each value doubled.
+For example:
+[1, 2, 3] --> [2, 4, 6]*/
+function doubleArray(arr){
+    let arrNew = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrNew.push(arr[i]*2);
+    }
+    return arrNew;
+}
+console.log(maps([1, 2, 3]));
+
+// another solution
+function maps(array){
+    return array.map(i => i*2);
+  }
+
+function maps(array) {
+    return array.map(function(num) { // callback function
+        return num *2;
+    });
+}
+console.log(maps([4, 8, 16]));
