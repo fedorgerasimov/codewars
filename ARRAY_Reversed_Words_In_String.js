9b0a864b8dd83fe3702f5753e94aca662a08f6a9
fedorgerasimov <1031577@mail.ru>
2022-02-03 11:27:13 +0300

    8kyu

--- ARRAY_Reversed_Words_In_String.js
+++ ARRAY_Reversed_Words_In_String.js
@@ -11,15 +11,12 @@ function reverseWords(str) {
 console.log(reverseWords("The greatest victory is that which requires no battle"));
 
 
-function reverseWords(str){  // swap(reverse) all characters in a string but not words
-    let newStr = str.split('').reverse().join('');
-    return newStr;
-}
-console.log("The greatest victory is that which requires no battle"); 
+const reverseWords1 = str => str.split(' ').reverse().join(' '); // ARROW FUNCTION
+console.log(reverseWords1("The greatest victory is that which requires no battle"));
 
-let str = "The greatest victory"; // swap(reverse) all characters in a string but not words
-let newStr = '';
-for (let i = str.length -1; i >= 0; i--){
-    newStr += str[i];
-}
-console.log(newStr);
+
+
+
+
+
+
--- Keep_Hydrated.js
+++ Keep_Hydrated.js
@@ -12,5 +12,4 @@ time = 11.8--> litres = 5
 function litres(time) {
     return Math.floor(time * 0.5);
 }
-
 const litres1 = time => Math.floor(time * 0.5);
