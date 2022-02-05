328293d421926649cdb023d7fbca5e228ccc5c60
fedorgerasimov <1031577@mail.ru>
2022-02-05 19:23:34 +0300

    8kyu

---
+++ Convert_Boolean_to_String.js
@@ -0,0 +1,20 @@
+/*Implement a function which convert
+ the given boolean value into its string representation.
+Note: Only valid inputs will be given.
+ */
+function booleanToString(b){
+    if (b === true) {
+        return `${true}`;
+    }
+    return `${false}`;
+}
+
+function booleanToString(b){
+    return b.toString();
+}
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+}
+
+const booleanToString1 = b => (b === true) ? 'true' : 'false';
