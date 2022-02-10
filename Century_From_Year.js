b007fd7c650f58fa099f4ca94527e1604341c265
fedorgerasimov <1031577@mail.ru>
2022-02-10 22:32:00 +0300

    8kyu

--- Century_From_Year.js
+++ Century_From_Year.js
@@ -1,7 +1,7 @@
 /*Introduction
 The first century spans from the year 1 up to and including the year 100, 
 the second century - from the year 101 up to and including the year 200, etc.
-Task
+JavaScript_For_Beginners
 Given a year, return the century it is in.
 Examples
 1705 --> 18
