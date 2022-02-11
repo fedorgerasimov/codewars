4dda113972ae7e2d1f230dee11a7713cbc9d8a8e
fedorgerasimov <1031577@mail.ru>
2022-02-11 21:58:23 +0300

    8kyu

---
+++ Opposites_Attract.js
@@ -0,0 +1,23 @@
+/*Timmy & Sarah think they are in love, but around where they live,
+they will only know once they pick a flower each.
+If one of the flowers has an even number of petals
+and the other has an odd number of petals it means they are in love.
+Write a function that will take the number of petals of each flower
+ and return true if they are in love and false if they aren't.*/
+
+function fellInLove(flower1, flower2){
+    if ((flower1 % 2 == 0 && flower2 % 2 == 1) || (flower2 % 2 == 0 && flower1 % 2 == 1)){
+        return true
+    }
+    return false
+}
+
+
+function fellInLove(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2;
+}
+
+
+function fellInLove(flower1, flower2){
+    return (flower1 + flower2) % 2 === 1  //  (odd + even) % 2 === 1
+}
